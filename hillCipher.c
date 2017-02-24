aa8ddbf1c85fb94782af2a97655b9b891cde2848
Luis <luisivan001@knights.ucf.edu>
2017-02-24 10:08:08 -0500

    final
    just had to free everything

--- hillCipher.c
+++ hillCipher.c
@@ -1,7 +1,8 @@
 // Luis Ivan
-// luisivan001@knights.ucf.edu
 
-// This program basically converts plain text in to encrypted text
+// Hill Cipher 
+
+// This program converts plain text in to encrypted text
 // when provided with a cipher key of "n" length.
 // "n" can not be any larger than 9 or smaller than 2
 
@@ -17,7 +18,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DEBUG 0
 
 
 
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	keyBuffer=malloc(sizeof(int)*capacity);
 	ptextBuffer=malloc(sizeof(char)*charCap);
 	
-	//keyBuffer = malloc(sizeof(char)*1000);
+
 	// encryption key file will store a single positive integer,
 	// n (1 < n < 10), number of rows and columns in matrix
 	// "r" = open for reading 
@@ -54,15 +54,12 @@ int main(int argc, char *argv[])
 	
 	// character array of size 10000,
 	// the text files no more than 9991 letters 
-	
-	// punctuation, numbers, special characters, and whitespace
 	if ((ptextFile = fopen(argv[2], "r")) == NULL)
 	{
 		fprintf(stderr, "Could not open %s in main()!\n", argv[2]);
 		exit(0);
 	}
 	
-	// lets make this a matrix
 	// stores the key	
 	while (fscanf(eKeyFile, "%d" , keyBuffer+size)!=EOF)
 	{ 
@@ -77,9 +74,6 @@ int main(int argc, char *argv[])
 		keyBuffer[j]=-1;
     }
 	
-	
-
-    
     i=0;
     
     // takes in every character and determines if letter
@@ -103,24 +97,6 @@ int main(int argc, char *argv[])
 		
 	index=i;
 	
-	/*
-	
-	// this is too double check the last char in the buffer
-	// a non letter could have slipped in
-	if (temp<97||temp>123)
-	{
-		ptextBuffer[index]=0;
-		ptextBuffer[index]='x';
-		index++;
-	}
-	
-	*/
-	// *** double check with the 2 key ***
-	// *** at the 3 key it does not insert the leading x ***
-	// *** what happens at the 4 key ***
-	// if(index%key!=0)
-	// index++;
-	// text[index]='x';
 	// we have to make sure that the index is a factor
 	// of key[0] if not we need to padd with x's ptextBuffer
 	// index is how many letters taken in
@@ -145,21 +121,20 @@ int main(int argc, char *argv[])
 	// this is supposed to do matrix multiplication Hill style
 	while(count<index)
 	{
-		// rows
+		// rows of the matrix
 		for(row=0;row< key;row++)
 		{
 			
 			
-			// columns
+			// columns of the matrix
 			k=count;
 			for(col=0;col< key;col++)
 			{	
 			
 				if(ecount==(key*key+1))
 					ecount=1;
-					
-					
-					e=(int)e+((int)ptextBuffer[k++]-97)*(keyBuffer[ecount]);
+						
+				e=(int)e+((int)ptextBuffer[k++]-97)*(keyBuffer[ecount]);
 			
 				ecount++;
 				
@@ -168,7 +143,8 @@ int main(int argc, char *argv[])
 						
 			}
 				
-			//mod26
+			// mod26 makes sure the text is from 0 to 25
+			// which correlates to the letters of the alphabet 
 			etext[row+count]=e%26+'a';
 		
 			e=0;
@@ -179,6 +155,7 @@ int main(int argc, char *argv[])
 			
 	}
 	
+	// prints out the key
 	printf("\nKey matrix:\n\n");
 	
 	for(i=1;i<size;i++)
@@ -217,8 +194,11 @@ int main(int argc, char *argv[])
 	
 	printf("\n\n");
 	
-
-	
+fclose(eKeyFile);
+fclose(ptextFile);
+free(keyBuffer);
+free(ptextBuffer);
+free(etext);
 	
 return 0; 
 }
